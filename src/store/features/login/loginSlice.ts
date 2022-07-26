9136179a65d85e89e3f7cc960b53bd6e42e0ad17
Alex <tendren3310@googlemail.com>
2022-07-26 11:16:22 +0300

    get data user and set State

--- src/store/features/login/loginSlice.ts
+++ src/store/features/login/loginSlice.ts
@@ -1,8 +1,8 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { loginAPI } from 'api';
 import { FormikInitialType } from 'pages';
-import { LoginType, AppDispatch } from 'store';
+import { LoginType, AppDispatch, AuthType } from 'store';
 
 const initialState: LoginType = {
   _id: null,
@@ -18,27 +18,33 @@ export const loginUserThunk = createAsyncThunk<
   void,
   FormikInitialType,
   { dispatch: AppDispatch }
->('login/loginUserThunk', async data => {
+>('login/loginUserThunk', async (data, thunkAPI) => {
   const response = await loginAPI.login(data);
-  console.log(response);
+  thunkAPI.dispatch(setUserData(response.data));
 });
 
-export const isInitializedAppThunk = createAsyncThunk<void, void>(
-  'login/isInitializedAppThunk',
-  async (_, thunkAPI) => {
-    try {
-      await loginAPI.authMe();
-    } catch (e) {
-      thunkAPI.fulfillWithValue(e);
-    }
-  },
-);
+export const isInitializedAppThunk = createAsyncThunk<
+  void,
+  void,
+  { dispatch: AppDispatch }
+>('login/isInitializedAppThunk', async (_, thunkAPI) => {
+  try {
+    const res = await loginAPI.authMe();
+    thunkAPI.dispatch(setUserData(res.data));
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 export const loginSlice = createSlice({
   name: 'login',
   initialState,
   reducers: {
-    // setAuthUserData: (_, action: PayloadAction<any>): any => action,
+    setUserData: (_, action: PayloadAction<AuthType>): LoginType => ({
+      ...action.payload,
+      isLoggedIn: true,
+      isInitialized: true,
+    }),
     // changeFetching: (state, action: PayloadAction<any>): any => action,
   },
   extraReducers: builder => {
@@ -48,17 +54,15 @@ export const loginSlice = createSlice({
       })
       .addCase(loginUserThunk.fulfilled, (state, action) => {
         console.log(action);
-        state.isLoggedIn = true;
       })
       .addCase(loginUserThunk.rejected, (state, action) => {
-        console.log(action);
+        console.log(action.error);
       })
       .addCase(isInitializedAppThunk.pending, (state, action) => {
         console.log(action);
       })
       .addCase(isInitializedAppThunk.fulfilled, (state, action) => {
         console.log(action);
-        state.isInitialized = true;
       })
       .addCase(isInitializedAppThunk.rejected, (state, action) => {
         console.log(action.error);
@@ -66,4 +70,4 @@ export const loginSlice = createSlice({
   },
 });
 
-// export const {} = loginSlice.actions;
+export const { setUserData } = loginSlice.actions;
