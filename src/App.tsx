9f232d549458355bdd67f2977e72a4bc2e42a963
ToshaBY <anton.rybakou@gmail.com>
2022-08-02 21:00:42 +0300

    refactor: naming

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Container } from '@mui/material';
 import s from './App.module.css';
 
 import { HeadBar, NavBar, AppRoutes, CircularInitialized } from 'components';
-import { ErrorSnackbar } from 'components/ErrorSnackbar/ErrorSnackbar';
+import { ErrorSnackbar } from 'components/errorSnackbar/ErrorSnackbar';
 import { useAppDispatch, useAppSelector } from 'hooks/useTypeHooks';
 import { isInitializedAppThunk } from 'store';
 
