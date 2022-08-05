f541544d13ed32141e941943ac48ba29b80ac362
Alex <tendren3310@googlemail.com>
2022-08-05 18:00:55 +0300

    refactoring

--- src/components/searchInput/SearchInput.tsx
+++ src/components/searchInput/SearchInput.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ReactElement } from 'react';
+import { ChangeEvent, ReactElement, useEffect, useState } from 'react';
 
 import SearchIcon from '@mui/icons-material/Search';
 import { InputAdornment } from '@mui/material';
@@ -9,38 +9,61 @@ import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
-export const SearchInput = (): ReactElement => (
-  <Box>
-    <FormControl variant="standard" sx={{ width: '100%' }}>
-      <Typography
-        sx={{
-          fontFamily: 'Montserrat',
-          fontStyle: 'normal',
-          fontWeight: 500,
-          fontSize: '14px',
-          mb: '8px',
-        }}
-      >
-        Search
-      </Typography>
-      <Paper
-        component="form"
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100%' }}
-      >
-        <InputBase
-          size="small"
-          sx={{ padding: '5px', width: '100%' }}
-          startAdornment={
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          }
-          inputProps={{
-            style: { padding: '0' },
+import { useDebounce } from 'hooks/useDebounce';
+import { useAppDispatch, useAppSelector } from 'hooks/useTypeHooks';
+import { setParams } from 'store';
+
+export const SearchInput = (): ReactElement => {
+  const [value, setValue] = useState<string>('');
+  const debouncedValue = useDebounce<string>(value);
+  const params = useAppSelector(state => state.params);
+  const dispatch = useAppDispatch();
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setValue(event.target.value);
+  };
+  useEffect(() => {
+    dispatch(setParams({ ...params, packName: debouncedValue }));
+  }, [debouncedValue]);
+  return (
+    <Box>
+      <FormControl variant="standard" sx={{ width: '100%' }}>
+        <Typography
+          sx={{
+            fontFamily: 'Montserrat',
+            fontStyle: 'normal',
+            fontWeight: 500,
+            fontSize: '14px',
+            mb: '8px',
           }}
-          placeholder="Provide your text"
-        />
-      </Paper>
-    </FormControl>
-  </Box>
-);
+        >
+          Search
+        </Typography>
+        <Paper
+          component="form"
+          sx={{
+            p: '2px 4px',
+            display: 'flex',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <InputBase
+            size="small"
+            sx={{ padding: '5px', width: '100%' }}
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            }
+            inputProps={{
+              style: { padding: '0' },
+            }}
+            placeholder="Provide your text"
+            onChange={handleChange}
+          />
+        </Paper>
+      </FormControl>
+    </Box>
+  );
+};
