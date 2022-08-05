e7d803f9619722e8ef4f42af065ec3a67316b1fb
Alex <tendren3310@googlemail.com>
2022-08-05 19:06:44 +0300

    refactoring API params

--- src/api/packsAPI/packsAPI.ts
+++ src/api/packsAPI/packsAPI.ts
@@ -4,10 +4,10 @@ import { PacksType, ParamsType } from 'store';
 
 export const packsAPI = {
   getPacks(params: ParamsType) {
-    const { page, pageCount, min, max, packName, userId, sortPacks } = params;
-    return instance.get<PacksType>(
-      `${ENDPOINT.PACKS}/?page=${page}&pageCount=${pageCount}&min=${min}&max=${max}&packName=${packName}&user_id=${userId}&sortPacks=${sortPacks}`,
-      {},
-    );
+    return instance.get<PacksType>(`${ENDPOINT.PACKS}/`, {
+      params: {
+        ...params,
+      },
+    });
   },
 };
